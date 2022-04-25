39092afdc942e931bcbc7b666384283ecc22ebd8
eunju-baek <77427870+eunju-baek@users.noreply.github.com>
2022-04-25 18:11:17 +0900

    This file for summarizing of categorical variables

---
+++ Category_summary.R
@@ -0,0 +1,14 @@
+#This file for summarizing of categorical variables
+
+library(data.table)
+a <- fread("pheno.txt")
+li <- list()
+
+# For example, the columns of categorical variables are for 8 to 11 >> categorical column name:A and B
+
+for(i in 8:11){
+b <- a %>% group_by(A,B) ^>^ summarise(total = n())
+b <- b %>% mutate(percent = round(total/sum(total),2))
+li[[i]] <- b }
+
+list <- do.call("rbind",li)
