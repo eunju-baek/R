296fcd6b0ba3acd0d060b7c7b0cd268750b1c121
eunju-baek <77427870+eunju-baek@users.noreply.github.com>
2022-04-26 11:39:05 +0900

    Create Dummy_coding.R

---
+++ Dummy_coding.R
@@ -0,0 +1,7 @@
+library(fastDummies)
+
+#root data frame >> a
+
+a <- dummy_cols(.data=a,select_columns= c("X1329.0.0",  "X1339.0.0",  "X1349.0.0",  "X1359.0.0",  "X1369.0.0","X1379.0.0" , "X1389.0.0" , "X1408.0.0" , "X1478.0.0" , "X1558.0.0"),remove_first_dummy=FALSE)
+
+#combining for dummy columns
