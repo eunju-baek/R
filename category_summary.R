a30813aa4bf74d0bbbed454cc6ed33cd8e4a7f0c
eunju-baek <77427870+eunju-baek@users.noreply.github.com>
2022-04-26 14:14:47 +0900

    characteristics by categorical variables

--- category_summary.R
+++ category_summary.R
@@ -1,14 +1,18 @@
 #This file for summarizing of categorical variables
 
-library(data.table)
-a <- fread("pheno.txt")
 li <- list()
 
+library(data.table)
+a <- fread("pheno.txt")
+
+
 # For example, the columns of categorical variables are from 8 to 11 >> categorical column name:A and B
+
+# summarise a total number of samples and percentage stratified by categorical variables
 
 for(i in 8:11){
 b <- a %>% group_by(A,B) ^>^ summarise(total = n())
 b <- b %>% mutate(percent = round(total/sum(total),2))
-li[[i]] <- b }
+  li[[i]] <- b }
 
 list <- do.call("rbind",li)
