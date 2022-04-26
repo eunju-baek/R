d76bb22ea723e66d95ef863f380ad01614f5df19
eunju-baek <77427870+eunju-baek@users.noreply.github.com>
2022-04-26 14:11:53 +0900

    transform categorical variable to dummy variable

--- dummy_coding.R
+++ dummy_coding.R
@@ -1,6 +1,10 @@
 library(fastDummies)
+library(data.table)
+
 
 #root data frame >> a
+
+a <- fread("a")
 
 a <- dummy_cols(.data=a,select_columns= c("X1329.0.0",  "X1339.0.0",  "X1349.0.0",  "X1359.0.0",  "X1369.0.0","X1379.0.0" , "X1389.0.0" , "X1408.0.0" , "X1478.0.0" , "X1558.0.0"),remove_first_dummy=FALSE)
 
