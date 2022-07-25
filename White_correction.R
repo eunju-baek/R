fe12ae57da6b09a8d104b820dd9c88b5eb5ad0ca
eunju-baek <77427870+eunju-baek@users.noreply.github.com>
2022-07-25 10:00:50 +0900

    Update White_correction.R

--- White_correction.R
+++ White_correction.R
@@ -13,8 +13,3 @@ summary(m1)
 h1[]
 
 
-#optional loop
-
-
-for(i in env){for(j in 1:4){assign(paste("ori",i,sep=''),lm(bmi ~ V48+age+sex+pc1+pc2+pc3+pc4+pc5+pc6+pc7+pc8+pc9+pc10+array+b[,i]+(b[,i]*V48),data=b))}}
-for(i in env){paste("ori",i,sep='') %>% get() %>% vcovHC(.,type="HC3") %>% assign(paste("wt",i,sep=''),.)}
